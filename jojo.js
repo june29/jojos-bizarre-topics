1be38a444a6627eac77fdbc0288f8a7527a5d8e8
june29 <june29.jp@gmail.com>
2016-05-07 00:26:21 +0900

    Write HTML/CSS/JavaScript

---
+++ jojo.js
@@ -0,0 +1,31 @@
+$(function() {
+  SHUFFLE_COUNT = 20
+
+  function start(stands) {
+    $('#wrapper').click(function() {
+      shuffle(stands, SHUFFLE_COUNT);
+    });
+  }
+
+  function shuffle(stands, counter) {
+    if (counter < 1) { return }
+
+    var stand = _.sample(stands);
+
+    $('h1').text(stand.stand_name);
+    $('h2').text(stand.operator_name);
+
+    setTimeout(function() { shuffle(stands, counter - 1)}, 50);
+  }
+
+  function fit() {
+    $('#wrapper').width($(window).width()).height($(window).height());
+  }
+
+  fit();
+  $(window).resize(fit);
+
+  $.getJSON('/stands.json', function(data) {
+    start(data);
+  });
+});
